d4743f8c7954aae96a8d45a98c0c5d7e45df5740
JoaoGrais <115734114+JoaoGrais@users.noreply.github.com>
2023-04-05 15:13:21 -0300

    Add files via upload

---
+++ relogio.js
@@ -0,0 +1,31 @@
+function relogio(){
+    const espacoRelogio = document.querySelector(".relogio");
+    const horaAgora = new Date();
+
+    const horas = horaAgora.getHours();
+
+    const minutos = horaAgora.getMinutes();
+
+    const segundos = horaAgora.getSeconds();
+
+    const formatoHora = horas.toString().padStart(2, "0");
+    const formatoMinutos = minutos.toString().padStart(2, "0");
+    const formatoSegundos = segundos.toString().padStart(2, "0");
+
+    espacoRelogio.textContent = `${formatoHora}:${formatoMinutos}:${formatoSegundos}`;
+
+    const frase = document.querySelector('.frase')
+    dia = new Array ('Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado');
+
+    mes = new Array ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro');
+
+    hoje = new Date()
+
+    frase.innerHTML = `${dia[hoje.getDay()]}, ${hoje.getDate().toString().padStart(2, 0)} de ${mes[hoje.getMonth()]} de${hoje.getFullYear()}`
+
+
+}
+
+setInterval(relogio, 1000)
+
+
